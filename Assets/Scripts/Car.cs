b325e1040dc6ee27f2d8d01d97ffb82c3a5984af
nboehning <nboehning@gmail.com>
2017-11-13 17:56:24 -0600

    Created scripts
    Created scripts and created a dummy and player car prefab

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+
+    public string licensePlate;
+
+    private void Start()
+    {
+        GenerateLicensePlate();
+    }
+
+    private void GenerateLicensePlate()
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        StringBuilder builder = new StringBuilder();
+
+        int plateLength = Random.Range(6, 8);
+
+        for (int i = 0; i < plateLength; i++)
+        {
+            char c = chars[(int) Random.Range(0, 36)];
+            builder.Append(c);
+        }
+
+        licensePlate = builder.ToString();
+    }
+}
---
+++ Assets/Scripts/ParkingSpotSensor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParkingSpotSensor : MonoBehaviour
+{
+
+	public Light sensorLight;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Car")
+		{
+			Debug.Log("Car entered the parking spot");
+			sensorLight.color = Color.red;
+			//string plate = other.GetComponent<Car>().lisencePlate;
+			//Search customer DB for this license plate
+			// If it exists
+				// Do nothing
+			// Else
+				// Customer newCustomer = new Customer(Time.time, plate);
+				// Add newCustomer to customer DB (base.addCustomer(newCustomer))
+			
+		}
+		else if(other.tag != "Car")
+		{
+			Debug.Log("Something besides car entered");
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "Car")
+		{
+			Debug.Log("Car exited the parking spot");
+			sensorLight.color = Color.green;
+		}
+		else if(other.tag != "Car")
+		{
+			Debug.Log("Something besides car exited");
+		}
+	}
+}

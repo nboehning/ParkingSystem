c6e8154748c15cfb0ca5a19d7dbeaa5a6a5a6d3a
Joseph Davis <soccermaniac898@gmail.com>
2017-11-15 17:50:53 -0600

    Add C# code

--- Assets/Scripts/Sensor.cs
+++ Assets/Scripts/Sensor.cs
@@ -4,13 +4,6 @@ using UnityEngine;
 
 public class Sensor : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    protected static List<Customer> customers = new List<Customer>();
+
 }
